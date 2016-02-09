bc827532dfa7435306d9fef673a4b43dec62dab5
mbMosman <mbmosman@users.noreply.github.com>
2016-02-08 18:00:38 -0600

    first commit

---
+++ src/edu/htc/tictactoe/Main.java
@@ -0,0 +1,11 @@
+package edu.htc.tictactoe;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        TicTacToe game = new TicTacToe();
+        game.playGame();
+
+    }
+}
---
+++ src/edu/htc/tictactoe/TicTacToe.java
@@ -0,0 +1,9 @@
+package edu.htc.tictactoe;
+
+public class TicTacToe {
+
+  public void playGame() {
+    
+  }
+
+}
